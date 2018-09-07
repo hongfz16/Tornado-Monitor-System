5becd608702f8f7784944427d636e8fa82f9b164
hongfz <hongfz16@163.com>
2018-09-07 19:26:57 +0800

    add schema.sql

--- start.py
+++ start.py
@@ -16,9 +16,68 @@ import unicodedata
 
 from tornado.options import define, options
 
+with open('secret.json','w') as f:
+    db_data = json.load(f)
+    define("db_host", default="127.0.0.1", help="database host")
+    define("db_port", default=5432, help="database port")
+    define("db_database", default=db_data['Database'], help="database name")
+    define("db_user", default=db_data['Username'], help="database user")
+    define("db_password", default=['Password'], help="database password")	
+
 define("port", default=8888, help="run on the given port", type=int)
-define("db_host", default="127.0.0.1", help="blog database host")
-define("db_port", default=5432, help="blog database port")
-define("db_database", default="tornado_blog", help="blog database name")
-define("db_user", default="tornado", help="blog database user")
-define("db_password", default="ttasw1234", help="blog database password")
+
+async def maybe_create_tables(db):
+    try:
+        with (await db.cursor()) as cur:
+            await cur.execute("SELECT COUNT(*) FROM entries LIMIT 1")
+            await cur.fetchone()
+    except psycopg2.ProgrammingError:
+        with open('schema.sql') as f:
+            schema = f.read()
+        with (await db.cursor()) as cur:
+            await cur.execute(schema)
+
+class Application(tornado.web.Application):
+    def __init__(self, db):
+        self.db = db
+        handlers = [
+            (r"/", HomeHandler),
+            (r"/auth/signup", AuthSignupHandler),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler),
+        ]
+        settings = dict(
+            web_title=u"Intelligent Monitor System",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            ui_modules={},
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            login_url="/auth/login",
+            debug=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+async def main():
+    tornado.options.parse_command_line()
+
+    # Create the global connection pool.
+    async with aiopg.create_pool(
+            host=options.db_host,
+            port=options.db_port,
+            user=options.db_user,
+            password=options.db_password,
+            dbname=options.db_database) as db:
+        await maybe_create_tables(db)
+        app = Application(db)
+        app.listen(options.port)
+
+        # In this demo the server will simply run until interrupted
+        # with Ctrl-C, but if you want to shut down more gracefully,
+        # call shutdown_event.set().
+        shutdown_event = tornado.locks.Event()
+        await shutdown_event.wait()
+
+
+if __name__ == "__main__":
+    tornado.ioloop.IOLoop.current().run_sync(main)
