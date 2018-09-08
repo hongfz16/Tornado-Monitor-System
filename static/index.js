5aa6db7ce522fa8417dae6025a6fcc6b270e8b99
hongfz <hongfz16@163.com>
2018-09-08 23:29:09 +0800

    finish reconstructing codes

---
+++ static/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	if ("WebSocket" in window) {
+		var ws_warning_path = 'ws://' + window.location.host + '/warning_websocket';
+		var ws_warning = new WebSocket(ws_warning_path);
+		ws_warning.onopen = function() {
+			ws_warning.send(1);
+            console.log("First sending request!")
+		};
+		ws_warning.onmessage = function(msg) {
+			$("#wswarning").text(msg.data);
+			ws_warning.send(1);
+            console.log("Sending request!")
+		};
+		ws_warning.onerror = function(e) {
+			console.log(e);
+			ws_warning.send(-1);
+		};
+	} else {
+		alert("WebSocket not supported");
+	}
+});
+window.onbeforeunload = function() {
+    ws_warning.send(-2)
+    ws_warning.onclose = function () {
+        console.log('Closing...');
+    }; // disable onclose handler first
+    ws_warning.close()
+};
